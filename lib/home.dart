531613c5d1a13b2a86b48760c1c5a633d590eafa
sarankumar02 <sarandhanush123@gmail.com>
2024-04-07 15:51:55 +0530

    complete

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: avoid_unnecessary_containers
 
 import 'dart:math';
-
+//
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -172,15 +174,12 @@ class _HomeState extends State<Home> {
                 child: ElevatedButton(
                     onPressed: () {
                       if (text.text != '' && trail.text != '') {
-                        print("Okkk");
-
                         var rng = Random().nextInt(200);
 
                         var ok = dbHelper.insertTask(
                             note(mm: text.text, id: rng, descrip: trail.text));
-                        print(ok);
-                        // var nn = _refreshTaskList();
 
+                        _refreshTaskList();
                         text.clear();
                         trail.clear();
                       }
@@ -226,4 +225,84 @@ class _HomeState extends State<Home> {
   }
 }
 
-//
+class DatabaseHelper {
+  Database? _database;
+
+  // DatabaseHelper() {
+  //   initDatabase();
+  // }
+
+  Future<void> initDatabase() async {
+    if (_database != null) return;
+
+    _database = await openDatabase(
+      join(await getDatabasesPath(), 'tasks.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE note(id INTEGER PRIMARY KEY AUTOINCREMENT, mm TEXT, descrip TEXT)",
+        );
+      },
+      version: 1,
+    );
+  }
+
+  Future<int> insertTask(note note) async {
+    await initDatabase();
+
+    return await _database!.insert('note', note.toMap());
+  }
+
+  Future<int> updatenote(note note) async {
+    print("upda");
+    return await _database!.update(
+      'note',
+      note.toMap(),
+      where: 'id = ?',
+      whereArgs: [note.mm],
+    );
+  }
+
+  Future<void> deletenot(int id) async {
+    // Get a reference to the database.
+    // ignore: await_only_futures
+    final db = await _database;
+
+    await db?.delete(
+      'note',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<List> getTasks() async {
+    await initDatabase();
+    final List<Map<String, dynamic>> maps = await _database!.query('note');
+    return List.generate(maps.length, (i) {
+      return note(
+          id: maps[i]['id'], mm: maps[i]['mm'], descrip: maps[i]['descrip']);
+    });
+  }
+}
+
+// ignore: camel_case_types
+class note {
+  String mm;
+  int id;
+  String descrip;
+  note({
+    required this.mm,
+    required this.id,
+    required this.descrip,
+  });
+  Map<String, dynamic> toMap() {
+    // ignore: pre
+
+    var map = <String, dynamic>{};
+
+    map['descrip'] = descrip;
+    map['mm'] = mm;
+    map['id'] = id;
+
+    return map;
+  }
+}
